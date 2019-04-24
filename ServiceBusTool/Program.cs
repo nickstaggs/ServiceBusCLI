a220bf5821c8b4bf85e03e275a940a17e90019bb
Nick Staggs <nick.c.staggs@gmail.com>
2019-04-24 10:10:36 -0400

    Add project files.

---
+++ ServiceBusTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBusTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
